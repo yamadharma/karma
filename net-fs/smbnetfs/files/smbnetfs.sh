1e21d4b8beacd459f36e9bdccebf2b5f9f0c3d53
Dmitry S. Kulyabov <yamadharma@gmail.com>
2009-10-28 10:06:02 +0300

    Cleanups

--- net-fs/smbnetfs/files/smbnetfs.sh
+++ net-fs/smbnetfs/files/smbnetfs.sh
@@ -1,15 +1,5 @@
-#!/bin/sh
-
-FUSE_OPTIONS="-o allow_other"
-
-MY_ID=`id -u`
-MY_MOUNT=`grep -E "smbnetfs.*user_id=$MY_ID" /proc/mounts`
-
-if [ -z "$MY_MOUNT" -a -f ~/.smb/smbnetfs.conf ]
-    then
-    if [ ! -d ~/net/smb ]
-	then
-	mkdir -p ~/net/smb
-    fi    
-    /usr/bin/smbnetfs ${FUSE_OPTIONS} ~/net/smb > /dev/null 2>&1
-fi    
+if [ "$EUID" != "0" -a -f ~/.smb/smbnetfs.conf -a \
+     -z "$(grep -w "smbnetfs.*user_id=$MY_ID" /proc/mounts)" ] ; then
+	[ ! -d ~/net ] && mkdir -p ~/net
+	/usr/bin/smbnetfs ~/net > /dev/null 2>&1
+fi
