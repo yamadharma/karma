3734d7ccb3474f1509d5bf2ba7d8eb473b840369
Dmitry S. Kulyabov <yamadharma@gmail.com>
2016-12-25 17:34:53 +0300

    Вс дек 25 17:34:53 MSK 2016

--- net-fs/smbnetfs/files/smbnetfs.sh
+++
@@ -1,5 +0,0 @@
-if [ "$EUID" != "0" -a -f ~/.smb/smbnetfs.conf -a \
-     -z "$(grep -w "smbnetfs.*user_id=$MY_ID" /proc/mounts)" ] ; then
-	[ ! -d ~/net ] && mkdir -p ~/net
-	/usr/bin/smbnetfs ~/net > /dev/null 2>&1
-fi
