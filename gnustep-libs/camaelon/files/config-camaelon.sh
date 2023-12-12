4956671cdf23b4c1e458feb1efa30592aacae400
Dmitry S. Kulyabov <yamadharma@gmail.com>
2023-12-12 16:34:26 +0300

    Вт 12 дек 2023 16:34:26 MSK

--- gnustep-libs/camaelon/files/config-camaelon.sh
+++
@@ -1,4 +0,0 @@
-echo "Applying Camaelon default theme..."
-echo "defaults write NSGlobalDomain GSAppKitUserBundles \"(${GNUSTEP_SYSTEM_ROOT}/Library/Bundles/Camaelon.themeEngine)\""
-defaults write NSGlobalDomain GSAppKitUserBundles "(${GNUSTEP_SYSTEM_ROOT}/Library/Bundles/Camaelon.themeEngine)"
-
